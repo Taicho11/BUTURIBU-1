e803535f38259c717c3d3029e9083ae784014467
taichou <0ag2r33102-0t7c@au.com>
2022-09-08 12:42:01 +0900

    systemcriateに欠けていたメインのスクリプトを追加

--- foldermaking.py
+++
@@ -1,3 +0,0 @@
-import os
-for i in range(10):
-    os.makedirs(f'./{i}.taichou')
---
+++ systemcriate/lerning.py
@@ -0,0 +1,18 @@
+OUTPUTFOLDER    =
+ESTIMATEFOLDER  =
+HISTORYFILE     =
+INFOFILE        =
+CHANNEL         =
+INPUTSIZE       =
+FILTERNUMBER    =
+FILTERSIZE      =
+POOL            =
+DENSEUNIT       =
+LR              =
+INPUTSIZE       =
+BATCH           =
+EPOCKS          =
+VALIDRATE       =
+
+MODEL= model(outfolder=OUTPUTFOLDER,estimatefolder=ESTIMATEFOLDER,infofile=INFOFILE,historyfile=HISTORYFILE,channel=CHANNEL,inputsize=INPUTSIZE,filternumber=FILTERNUMBER,filtersize=FILTERSIZE,pool=POOL,denseunit=DENSEUNIT,lr=LR,imagesize=IMAGESIZE,batch=BATCH,epochs=EPOCHS,validrate=VALIDRATE)
+MODEL.control()
